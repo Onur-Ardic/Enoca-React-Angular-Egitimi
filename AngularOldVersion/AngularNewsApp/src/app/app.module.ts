fa13acb9745055808a24b8e81413c22edb6c643d
Onur Ardıç <onurardc@outlook.com>
2024-05-06 02:41:41 +0300

    api alındı

--- AngularOldVersion/AngularNewsApp/src/app/app.module.ts
+++ AngularOldVersion/AngularNewsApp/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,13 +13,20 @@ import { NewscardComponent } from './components/newscard/newscard.component';
     AppComponent,
     HeaderComponent,
     SliderComponent,
-    NewscardComponent
+    NewscardComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule], // HttpClientModule'yi buraya ekleyin
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private http: HttpClient) {
+    this.http
+      .get(
+        'https://newsapi.org/v2/top-headlines?country=tr&apiKey=fb44ce1bd88740d4990d843834598291'
+      )
+      .subscribe((data) => {
+        console.log(data);
+      });
+  }
+}
