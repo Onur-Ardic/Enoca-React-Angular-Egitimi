ae6face7158b6f2b591adfcc09fbb532524eb45c
Onur Ardıç <onurardc@outlook.com>
2024-05-15 01:58:37 +0300

    Kategorilere göre api çekme tamamlandı
    --Yapılacaklar--
    1. Routing yapısı kurulacak
    2. her bir filmin detayına gidecek
    3. Slider Eklenecek

--- Angular/AngularMovieApp/src/app/Components/movie-card/movie-card.component.ts
+++ Angular/AngularMovieApp/src/app/Components/movie-card/movie-card.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, OnInit } from '@angular/core';
+export interface Movie {
+  title: string;
+  poster_path: string;
+}
 
 @Component({
   selector: 'app-movie-card',
   templateUrl: './movie-card.component.html',
   styleUrls: ['./movie-card.component.scss'],
 })
-export class MovieCardComponent {
+export class MovieCardComponent implements OnInit {
+  movieData: Movie[] = [];
+
   constructor() {}
+
+  ngOnInit(): void {}
+
+  fetchData(category: string) {
+    let apiUrl = '';
+    if (category === 'now_playing') {
+      apiUrl =
+        'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1&api_key=db47e7b14bef3cdd12f9aa38d686e5c5';
+    }
+    if (category === 'upcoming') {
+      apiUrl =
+        'https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1&api_key=db47e7b14bef3cdd12f9aa38d686e5c5';
+    } else if (category === 'top_rated') {
+      apiUrl =
+        'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1&api_key=db47e7b14bef3cdd12f9aa38d686e5c5';
+    } else if (category === 'popular') {
+      apiUrl =
+        'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1&api_key=db47e7b14bef3cdd12f9aa38d686e5c5';
+    }
+
+    fetch(apiUrl)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        this.movieData = data.results;
+
+        console.log(this.movieData);
+      });
+  }
 }
--- Angular/AngularMovieApp/src/app/app.module.ts
+++ Angular/AngularMovieApp/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HeaderComponent } from './Components/header/header.component';
 import { HomeComponent } from './Components/home/home.component';
 import { MovieCardComponent } from './Components/movie-card/movie-card.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     HomeComponent,
     MovieCardComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
