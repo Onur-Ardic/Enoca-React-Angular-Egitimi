21beaadab4a40f03dc6f560e1661967cb0913356
Onur Ardıç <onurardc@outlook.com>
2024-05-16 18:41:38 +0300

    design fixed

--- Angular/AngularMovieApp/src/app/Components/navbar/navbar.component.ts
+++ Angular/AngularMovieApp/src/app/Components/navbar/navbar.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
-
+  search(event: Event): void {
+    event.preventDefault();
+    const searchInputValue = (event.target as HTMLFormElement)?.querySelector(
+      'input'
+    )?.value;
+    console.log('Aranan: ', searchInputValue);
+  }
 }
